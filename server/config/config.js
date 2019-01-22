4fa9b351ccae495750f8e0d4ae8b2204b0c1a669
Oscar Moncayo <oscar_mg7@hotmail.com>
2019-01-22 15:41:20 -0500

    Mongo URI added in heroku

--- server/config/config.js
+++ server/config/config.js
@@ -9,6 +9,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ============================
 // ======== DATA BASE  ========
 // ============================
-let urlDB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/db-coffee' : 'mongodb://user:123qwe@ds163764.mlab.com:63764/db-coffee';
+let urlDB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/db-coffee' : process.env.MONGO_URI;
 
 process.env.URLDB = urlDB;
